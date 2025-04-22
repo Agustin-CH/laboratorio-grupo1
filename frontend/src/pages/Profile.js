be205bcaf4cf13333f6d789c06ffaba62977a832
valengon1 <vgonzalez14@uade.edu.ar>
2025-04-22 19:35:42 -0300

    estrucutra general para el perfil

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,2 +1,21 @@
-/// Primera Prueba
-/// Segunda Prueba
+import React from "react";
+import { Box } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
+import ProfileHeader from "./ProfileHeader";
+import ProfileForm from "./ProfileForm";
+import ProfileFooter from "./ProfileFooter";
+
+const Profile = () => {
+  const isMobile = useMediaQuery("(max-width:600px)");
+
+  return (
+    <Box sx={{ fontFamily: "'Helvetica Neue', sans-serif" }}>
+      <ProfileHeader isMobile={isMobile} />
+      <ProfileForm isMobile={isMobile} />
+      <ProfileFooter />
+    </Box>
+  );
+};
+
+export default Profile;
