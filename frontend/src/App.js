7dd76ddeb793f30edad07aebc44abdab8fcb6548
FedericoFores <fedefores1234567@gmail.com>
2025-04-23 17:31:52 -0300

    ProductCatalog en proceso

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Container, Typography } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Container } from "@mui/material";
 import Home from "./pages/Home";
 import UserManagement from "./pages/UserManagement";
 import ProductCatalog from "./pages/ProductCatalog";
@@ -13,12 +13,16 @@ function App() {
     <Router>
       <Container maxWidth="md">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/usuarios" element={<UserManagement />} />
+          {/* Redirige la raíz al catálogo */}
+          <Route path="/" element={<Navigate to="/catalogo" replace />} />
+
+          {/* Vistas */}
           <Route path="/catalogo" element={<ProductCatalog />} />
+          <Route path="/usuarios" element={<UserManagement />} />
           <Route path="/carrito" element={<Cart />} />
           <Route path="/gestion-productos" element={<ProductManagement />} />
           <Route path="/mi-perfil" element={<Profile />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </Container>
     </Router>
