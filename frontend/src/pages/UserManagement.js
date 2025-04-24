46cd8e92cb8b58b22db6171698ef0be529c470cc
Matias Bordazahar <matbordazahar@uade.edu.ar>
2025-04-23 21:04:44 -0300

    Contenedor centrado para formulario del login

--- frontend/src/pages/UserManagement.js
+++ frontend/src/pages/UserManagement.js
@@ -1,4 +1,4 @@
-import { Box, Typography, useMediaQuery } from "@mui/material"
+import { Box, Typography, useMediaQuery, Paper } from "@mui/material"
 import PersonIcon from "@mui/icons-material/Person"
 
 const Login = () => {
@@ -22,6 +22,27 @@ const Login = () => {
         <Box sx={{ display: "flex", gap: 2 }}>
           <PersonIcon />
         </Box>
+      </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "80vh",
+          px: isMobile ? 2 : 4,
+        }}
+      >
+        <Paper
+          elevation={3}
+          sx={{
+            width: "100%",
+            maxWidth: 450,
+            p: 4,
+            borderRadius: 2,
+          }}
+        >
+        </Paper>
       </Box>
     </Box>
   )
