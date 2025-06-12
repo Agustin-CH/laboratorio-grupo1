56686470d9e06d105a22da338e67e760c58df5a1
valengon1 <vgonzalez14@uade.edu.ar>
2025-06-12 19:37:27 -0300

    Creo el service de order

--- backend/src/main/java/com/equipo1/ecommerce/backend/service/OrderService.java
+++ backend/src/main/java/com/equipo1/ecommerce/backend/service/OrderService.java
@@ -1,4 +1,20 @@
 package com.equipo1.ecommerce.backend.service;
 
-public class OrderService {
+import com.equipo1.ecommerce.backend.dto.OrderDTO;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface OrderService {
+
+    List<OrderDTO> getOrdersByUserId(Long userId);
+
+    @Transactional
+    OrderDTO createOrderFromCart(Long userId);
+
+    OrderDTO getOrderById(Long orderId);
+
+    @Transactional
+    OrderDTO cancelOrder(Long orderId);
 }
+
