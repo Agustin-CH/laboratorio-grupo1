8bfc107b6d7629a5a57a7937c33ad5bede0d3e21
valengon1 <vgonzalez14@uade.edu.ar>
2025-06-12 19:34:58 -0300

    Creo el order repository

--- backend/src/main/java/com/equipo1/ecommerce/backend/repository/OrderRepository.java
+++ backend/src/main/java/com/equipo1/ecommerce/backend/repository/OrderRepository.java
@@ -1,4 +1,11 @@
 package com.equipo1.ecommerce.backend.repository;
 
-public class OrderRepository {
+import com.equipo1.ecommerce.backend.model.Order;
+import com.equipo1.ecommerce.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUser(User user);
 }
