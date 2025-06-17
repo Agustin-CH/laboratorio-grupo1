f298ffb86e188ec49b920ba59f4f1e49288a5cd1
achiaravalli <agustin.chiaravalli@mercadolibre.com>
2025-06-16 21:59:10 -0300

    update product catalog

--- backend/src/main/java/com/equipo1/ecommerce/backend/dto/ProductDTO.java
+++ backend/src/main/java/com/equipo1/ecommerce/backend/dto/ProductDTO.java
@@ -1,5 +1,7 @@
+// src/main/java/com/equipo1/ecommerce/backend/dto/ProductDTO.java
 package com.equipo1.ecommerce.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -12,5 +14,7 @@ public class ProductDTO {
     private String imageUrl;
     private BigDecimal price;
     private Integer stock;
-    private Long categoryId; // si usás categoría
+
+    @JsonProperty("category")
+    private String categoryName;
 }
--- backend/src/main/java/com/equipo1/ecommerce/backend/model/Category.java
+++ backend/src/main/java/com/equipo1/ecommerce/backend/model/Category.java
@@ -1,9 +1,7 @@
 package com.equipo1.ecommerce.backend.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
@@ -15,12 +13,11 @@ import java.util.List;
 public class Category {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String name;
 
     @OneToMany(mappedBy = "category")
     private List<Product> products;
 }
-
